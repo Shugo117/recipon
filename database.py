a1def170faed20b009a866d0d5330ad827b506ab
hurap <hurappu117@outlook.com>
2026-02-20 08:49:16 +0900

    print database

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
+print("### DB URL =", DATABASE_URL)
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./recipon.db")
 
 # Renderのpostgres:// を修正
